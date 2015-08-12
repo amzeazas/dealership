149ec6217464a55a7e0a01cf648ecc62c9a2b07b
Alexa Zeazas & Travis Crampton <student@epicodus.com>
2015-08-12 09:34:45 -0700

    Add spec for id and cars.

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -11,4 +11,16 @@ describe(Dealership) do
       expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
+  describe('#id') do
+    it("returns the id of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+  describe('#cars') do
+    it("initially returns an empty array of cars for the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
 end

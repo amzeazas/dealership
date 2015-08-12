ec217727bbe125f94994edbc26e459d599f5c6e9
Alexa Zeazas & Travis Crampton <student@epicodus.com>
2015-08-12 10:05:11 -0700

    Add .find method.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -26,7 +26,18 @@ class Dealership
   define_method(:save) do
     @@dealerships.push(self)
   end
+
   define_singleton_method(:clear) do
     @@dealerships = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_dealership = nil
+    @@dealerships.each() do |dealership|
+      if dealership.id().eql?(id)
+        found_dealership = dealership
+      end
+    end
+    found_dealership
   end
 end

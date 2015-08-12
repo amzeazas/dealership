bc101b7e5fffe53df54ef60e4027b52bf702d253
Alexa Zeazas & Travis Crampton <student@epicodus.com>
2015-08-12 10:34:22 -0700

    Add vehicle methods.

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -22,7 +22,7 @@ describe('Vehicle') do
   end
   describe('#model') do
     it('returns the model of the vehicle') do
-      test_vehicle = Vehicle.new ("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
       test_vehicle.save()
       expect(test_vehicle.model()).to(eq("Prius"))
     end
